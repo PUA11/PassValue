99cd4e49e3a909740c8ae7f6407e574a90f21a69
pua <pua1199@163.com>
2016-01-08 15:54:41 +0800

    Initial Commit

---
+++ PassValue/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PassValue
+//
+//  Created by Pua on 16/1/8.
+//  Copyright © 2016年 Pua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PassValueTests/PassValueTests.m
@@ -0,0 +1,39 @@
+//
+//  PassValueTests.m
+//  PassValueTests
+//
+//  Created by Pua on 16/1/8.
+//  Copyright © 2016年 Pua. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PassValueTests : XCTestCase
+
+@end
+
+@implementation PassValueTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
